ef0de23d33d2032bbfab94ef342f7bba507a950d
Aditi Sharma <asharm29@guelph.ca>
2026-02-05 15:31:45 -0500

    folder structure added

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup (
-    name = "AI_Medical_Chatbot",
+    name = "AI_Medical_Chatbot",                    #project name
     version = '0.0.0',
     author = 'Aditi Sharma',
     author_email= 'aditii.01.shrm@gmail.com',
-    packages = find_packages(),
+    packages = find_packages(),                     #find init.py -> will be considered as the local package
     install_requires = []
 
 )
